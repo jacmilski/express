2d21a0a184b89b9e95b9cc4b875b035601ced306
jacmilski <jacmilski@gmail.com>
2021-02-26 12:27:47 +0100

    News filter front

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db: '',
+    db: 'mongodb+srv://Jacek-MDB:66uVV8jO988hAfCm@cluster1.21rta.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     keySession: ['TWOJLUCZ'],
     maxAgeSession: 24 * 60 * 60 * 1000,
 }
--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news');
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('news', { title: 'News' });
+  const search = req.query.search || '';
+  
+  const findNews = News
+    .find({title: new RegExp(search.trim(), 'i')}) // (string, forma)
+    .sort({ created: -1 }) //sortowanie malejąco to -1, rosnąco 1, domyślneto 0
+  findNews.exec((err, data) => { //metoda exec() bo metoda find przyjmie najpierw
+    //metody, które posortują dane i dopiero exec wykona find
+    res.render('news', { title: 'News', data, search });
+  });
+  
 });
 
 module.exports = router;
